58b453c666ebb6a5c2f68904678d67b2673d30d6
Eric Chen <ericchen1248@gmail.com>
2018-06-15 20:08:43 +0800

    Fix Angel's work inheritance

--- CitySimulator/Systems/Work/WorkSystem.cpp
+++ CitySimulator/Systems/Work/WorkSystem.cpp
@@ -1,12 +1,82 @@
 #include "WorkSystem.h"
+#include "Work.h"
+#include "WorkRule.h"
+#include "../../Controllers/CoreController.h"
 
-
+class WorkRule;
 
 WorkSystem::WorkSystem(): BaseSystem(WORK)
 {
 }
 
 
-WorkSystem::~WorkSystem()
+WorkSystem::~WorkSystem() = default;
+
+
+/**
+* \brief Registers a new work plot in the system
+* \param plot Plot to be registered
+*/
+void WorkSystem::Register(Plot* plot)
 {
+	(*plot).Register(new Work(plot));
+	BaseSystem::Register(plot);
 }
+
+/**
+* \brief Updates work system. Tally scores for work plots
+*/
+void WorkSystem::Update()
+{
+	/*
+	for (auto && plot : plots)
+	{
+		// Do this if you want to get the plot type (class work)
+		//const auto rule = dynamic_cast<Work*> (plot->GetPlotType());
+
+		// Tallying and adding score for occupant count. Positive for within limit people, negative for over
+		const auto count = plot->GetOccupantCount();
+		score += (std::min(count, maxOccupantCount) * scorePerOccupant - std::max(count - maxOccupantCount, 0) * overPenalty) * CoreController::Instance()->GetDeltaTime();
+	}
+	*/
+}
+
+/**
+* \brief Logs a citizen being satisified with a work
+* \param citizen Citzen being logged
+* \param rule Rule being logged
+*/
+void WorkSystem::LogSatisfied(Citizen* citizen, BaseRule* rule)
+{
+	// Dynamic cast rule to create a snapshot copy 
+	auto log = new Log(citizen->Coords(), new WorkRule(*dynamic_cast<WorkRule*>(rule)), citizen);
+	satisfiedLog.InsertLast(log);
+}
+
+
+/**
+* \brief Logs a citizen being unsatisified with a Work
+* \param citizen Citzen being logged
+* \param rule Rule being logged
+*/
+void WorkSystem::LogUnsatisfied(Citizen* citizen, BaseRule* rule)
+{
+	// Dynamic cast rule to create a snapshot copy 
+	auto log = new Log(citizen->Coords(), new WorkRule(*dynamic_cast<WorkRule*>(rule)), citizen);
+	unsatisfiedLog.InsertLast(log);
+}
+
+
+/**
+* \brief Resets the day (clears log & resets plots)
+*/
+void WorkSystem::ResetDay()
+{
+	for (auto && plot : plots)
+	{
+		plot->GetPlotType()->EndDay();
+	}
+	unsatisfiedLog.Dispose();
+	satisfiedLog.Dispose();
+}
+
--- CitySimulator/Systems/Work/WorkSystem.h
+++ CitySimulator/Systems/Work/WorkSystem.h
@@ -2,11 +2,17 @@
 #include "../Base/BaseSystem.h"
 
 
-class WorkSystem :
-    public BaseSystem
+class WorkSystem :  public BaseSystem
 {
 public:
     WorkSystem();
     ~WorkSystem();
+
+	void Register(Plot* plot);
+	void Update();
+	void LogSatisfied(Citizen* citizen, BaseRule* rule);
+	void LogUnsatisfied(Citizen* citizen, BaseRule* rule);
+	void ResetDay();
 };
+
 
