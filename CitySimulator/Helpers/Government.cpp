217ff5519cd1ed1dddefce7615cec7c4d8abf48a
Eric Chen <ericchen1248@gmail.com>
2018-07-03 12:18:22 +0800

    Birthrate getter

--- CitySimulator/Helpers/Government.cpp
+++ CitySimulator/Helpers/Government.cpp
@@ -5,17 +5,3 @@
 float Government::taxDollars = 0.f;
 float Government::birthRate = 0.3f;
 
-int Government::NewChildren(const int adultCount)
-{
-    const int thresh = static_cast<int>(birthRate * 100);
-    int birthCount = 0;
-    for (int i = 0; i < adultCount; ++++i)
-    {
-        const int rand = RandomInt(0, 100);
-        if (rand < thresh)
-        {
-            ++birthCount;
-        }
-    }
-    return birthCount;
-}
